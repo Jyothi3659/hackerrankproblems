4ea689779f7c27eeb49d0107541ba72918ba9a63
Jyothi <yedla.jyothi95@gmail.com>
2019-10-24 19:40:47 +0530

    committing after modified binary

--- decimal_binary.py
+++ decimal_binary.py
@@ -1,73 +1,75 @@
-"""Objective
-Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
-
-Task
-Given a base-
-integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in
-
-'s binary representation.
-
-Input Format
-
-A single integer,
-
-.
-
-Constraints
-
-Output Format
-
-Print a single base-
-integer denoting the maximum number of consecutive 's in the binary representation of
-
-.
-
-Sample Input 1
-
-5
-
-Sample Output 1
-
-1
-
-Sample Input 2
-
-13
-
-Sample Output 2
-
-2
-
-Explanation
-
-Sample Case 1:
-The binary representation of
-is , so the maximum number of consecutive 's is
-
-.
-
-Sample Case 2:
-The binary representation of
-is , so the maximum number of consecutive 's is ."""
+# # """Objective
+# Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+#
+# Task
+# Given a base-
+# integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in
+#
+# 's binary representation.
+#
+# Input Format
+#
+# A single integer,
+#
+# .
+#
+# Constraints
+#
+# Output Format
+#
+# Print a single base-
+# integer denoting the maximum number of consecutive 's in the binary representation of
+#
+# .
+#
+# Sample Input 1
+#
+# 5
+#
+# Sample Output 1
+#
+# 1
+#
+# Sample Input 2
+#
+# 13
+#
+# Sample Output 2
+#
+# 2
+#
+# Explanation
+#
+# Sample Case 1:
+# The binary representation of
+# is , so the maximum number of consecutive 's is
+#
+# .
+#
+# Sample Case 2:
+# The binary representation of
+# is , so the maximum number of consecutive 's is ."""
 def binary_func(n):
     binary_string = bin(n).replace("0b", "")
-    print(binary_string)
     previous = binary_string[0]
     final_count = 0
     count = 0
+    print(binary_string)
     for each in binary_string:
         if (each != 0 and count == 0):
             count += 1
             previous = each
+            print(count)
             if final_count < count:
                 final_count = count
-        elif each != 0 and previous == each:
+        elif (each != 0 and previous == each):
             previous = each
             count += 1
+            print(count)
             if final_count < count:
                 final_count = count
         else:
             previous = each
             count = 0
     return final_count
-# print(binary_func(439))
+print(binary_func(8))
--- decimal_binary_tests.py
+++ decimal_binary_tests.py
@@ -3,7 +3,7 @@ from decimal_binary import binary_func
 
 
 class DecimalBinaryTests(unittest.TestCase):
-    def test_binary(self):
+    def test_case1(self):
         """
             case for same values any number of times
         :return:
@@ -11,7 +11,7 @@ class DecimalBinaryTests(unittest.TestCase):
         # slot = ["01:00:00 02:00:00", "03:00:00 04:00:00", "04:00:00 12:00:00", "15:00:00 24:00:00"]
         slot = 5
         self.assertEqual(binary_func(slot), 2)
-    def test_binary(self):
+    def test_case2(self):
         """
             case for same values any number of times
         :return:
@@ -20,7 +20,7 @@ class DecimalBinaryTests(unittest.TestCase):
         slot = 8
         self.assertEqual(binary_func(slot), 3)
 
-    def test_binary(self):
+    def test_case3(self):
         """
 
         :return:
@@ -29,7 +29,7 @@ class DecimalBinaryTests(unittest.TestCase):
         slot = 439
         self.assertEqual(binary_func(slot), 3)
 
-    def test_binary(self):
+    def test_case4(self):
         """
             case for same values any number of times
         :return:
