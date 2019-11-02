838dc893527f8759ec38b49f4e1a68f3e8fbcad2
yeswanth <yeswanthcse2013@gmail.com>
2019-11-02 15:22:09 +0530

    modified if condition

--- Divisiblesumpairs.py
+++ Divisiblesumpairs.py
@@ -2,7 +2,7 @@ def divisibleSumPairs(k, ar):
     count=0
     for i in range(len(ar)):
         for j in range(i+1,len(ar)):
-            if((ar[i]+ar[j])%k==0):
+            if(ar[i]+ar[j])%k==0:
                 count+=1
     return count
 
